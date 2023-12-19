2f3ab0679dbb98b17a57084b5c675ff36e2c0df5
Samantha S <sami.berlynn@proton.me>
2023-12-19 19:46:59 +0100

    Caesar Cipher Project Added

--- projects/caesar_cipher.rb
+++ projects/caesar_cipher.rb
@@ -3,54 +3,29 @@
 # x + shift is < 97 (a) wrap
 # x isnt a letter - return original
 # x + shift is between 97 & 122
+# x + shift is between 65 & 90
 # Uppercase and lowercase
 # 65 - 90 A - Z
 # 97 - 122 a - z
 
-
-
-# def caesar_cipher(str, shift)
-#    arr = str.split('')
-#    arr = arr.map do |x| 
-#     if x.ord > 122 || x.ord < 97 
-#         p x.ord
-#         x #return whatever non a-z character this was
-#     elsif x.ord + shift > 122 
-#         p "xshift is > 122 #{x}"
-#         # start back at a, remove one from shift since we started over
-#         x = x.ord + shift - 26
-#         x.chr #turn back into char
-        
-
-#     elsif x.ord + shift < 97
-#         p "xshift is < 97"
-#         x = x.ord + shift + 26
-#         x.chr
-#     else 
-#         p 'xshift is < 122'
-#         (x.ord + shift).chr 
-#     end 
-#    end
-#    p arr.join()
-# end
-
 def caesar_cipher(str, shift)
     arr = str.split('')
     arr = arr.map do |x| 
+    # Uppercase letter    
     if x.ord >= 65 && x.ord <= 90 
         if x.ord + shift > 90 
-         p "xshift is > 90 #{x}"
-         # start back at a, remove one from shift since we started over
+        #  p "xshift is > 90 #{x}"
          x = x.ord + shift - 26
-         x.chr #turn back into char 
+         x.chr 
 
         elsif x.ord + shift < 65
-         p "xshift is < 65"
+        #  p "xshift is < 65"
          x = x.ord + shift + 26
          x.chr
         else 
          (x.ord + shift).chr 
         end 
+    # Lowercase Letter
     elsif x.ord >= 97 && x.ord <= 122
         if x.ord + shift > 122
            x = x.ord + shift - 26
@@ -61,18 +36,16 @@ def caesar_cipher(str, shift)
         else 
             (x.ord + shift).chr
         end
+    # Not a letter
     else
         p x.ord
-        x #return whatever non a-z character this was
+        x 
     end
 end
     p arr.join()
 end
 
-
-# dec = caesar_cipher("abc wxyz", 2)
-# caesar_cipher(dec, -2)
-
-two = caesar_cipher("A!~az Z 110for the win!`", -3)
- caesar_cipher(two, 3)
+# Tests
+# c = caesar_cipher("A!~az Z 110for the win!`", -3)
+# caesar_cipher(c, 3)
 
