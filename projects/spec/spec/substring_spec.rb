e4ecb41a19b2ad843fb1f5481a12e702e8d628e5
Samantha S <sami.berlynn@proton.me>
2023-12-20 21:15:36 +0100

    removed comments and extra function from substring spec test

--- projects/spec/spec/substring_spec.rb
+++ projects/spec/spec/substring_spec.rb
@@ -3,17 +3,15 @@ require_relative '../../substrings.rb'
 
 RSpec.describe 'Substring Exercises' do
   describe 'substring exercise' do
-    dictionary = Array.new(["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
+    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
     
     it 'returns hash with 2 substrings' do
-      
       result = substrings("below", dictionary)
       expected = { 'below' => 1, "low" => 1 }
       expect(expected).to eq(result)
     end
     
-    it 'returns hash with multiple substrings' do
-      # dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+    it 'returns hash with many substrings' do
       result = substrings("Howdy partner, sit down! How's it going?", dictionary)
       expected = { 
         "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, 
