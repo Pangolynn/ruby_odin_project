254ebbdd9786836a98c8cda8a987547903b9ca92
Samantha S <sami.berlynn@proton.me>
2024-02-23 16:02:01 +0100

    game works for all combinations, refactored code to be more 'rubylike', simplified methods and logic, added input checking and corresponding messaging, updated the way the board looks, added Game module as driver for game.

--- projects/tictactoe.rb
+++ projects/tictactoe.rb
@@ -1,42 +1,110 @@
-# Tic Tac Toe game
+# This class represents the game logic for Tic Tac Toe 
+#
+# To play a game, create a new instance of TicTacToe with 2 Player objects
+# and a board.
+# Its attr are game_over, turns, player_turn, players, and board
+# Use #setup to setup the Player's turns and symbols, and then #play to start the game
+# Example usage:
+#   t = TicTacToe.New
+#   t.setup
+#   t.play
 class Tictactoe
-  attr_accessor :game_over, :turns, :winner, :player_turn, :players
+  attr_accessor :game_over, :turns, :player_turn, :players, :board
 
   def initialize
     @player_turn = 0
     @turns = 0
-    @winner = ''
     @game_over = false
     @players = Array.new(2)
+    @board = Board.new
   end
 
-  def input_player_name
-    gets.chomp.to_s
+  def self.input_player_name
+    name = ''
+    name = gets.chomp.strip while name.empty?
+    name
+  end
+
+  def setup
+    puts "\nEnter player 1's name"
+    @players[0] = Player.new(Tictactoe.input_player_name)
+    puts "\nEnter player 2's name"
+    @players[1] = Player.new(Tictactoe.input_player_name)
+    @board.print_board
+    determine_symbol
+  end
+
+  def play
+    move = -1
+
+    if @player_turn == 1
+      puts "Player 1 #{@players[0].name}'s turn."
+      move = @players[0].move until @board.valid_move?(move)
+      @board.update_board(move,@players[0].sym)
+      @players[0].choices.push(move)
+      @turns += 1
+      @board.print_board
+      @board.winner?(@players[0].choices) ? (puts "#{@players[0].name} wins! Play Again?"; @game_over = true) : @player_turn = 2
+    else
+      puts "Player 2 #{@players[1].name}'s turn."
+      move = @players[1].move until @board.valid_move?(move)
+      @board.update_board(move, @players[1].sym)
+      @players[1].choices.push(move)
+      @turns += 1
+      @board.print_board
+      @board.winner?(@players[1].choices) ? (puts "#{@players[1].name} wins! Play Again?"; @game_over = true) : @player_turn = 1
+    end
+    return unless @turns >= 9 || @game_over
+
+    @game_over = true
+    puts "\nDraw! No winner :(. Play Again?" if @turns >= 9
+  end
+
+  private
+
+  # Decides which player goes first, and assigns a random symbol
+  def determine_symbol
+    first, second = @players.shuffle
+    first.sym, second.sym = %w[X O].shuffle
+    @player_turn = @players.index(first) + 1
+    puts "\nPlayer #{@player_turn} #{first.name} goes first with #{first.sym}"
   end
 end
 
+# This class represents a 3x3 Tic Tac Toe board
+#
+# It has a board attribute.
+# You can create a new board by calling Board.new
+# Example usage:
+#   board = Board.new
 class Board
-  attr_accessor :board
-
   def initialize
     @board = Array.new(9, '*')
   end
 
   def print_board
     @board.each_with_index do |x, i|
-      puts "\n" if (i % 3).zero?
-      print "#{x} "
+      print "\n - - - - - -\n|" if (i % 3).zero?
+
+      print " #{x} |"
     end
+    print "\n - - - - - -\n"
   end
 
   def update_board(move, sym)
-    p "#{move} #{sym}"
     @board[move] = sym
-    p @board[move]
   end
 
   def valid_move?(move)
-    @board[move] == '*'
+    # p move
+    if move >= 9 || move.negative?
+      puts 'Enter a number between 0-8'
+      return false
+    elsif @board[move] != '*'
+      puts 'That spot is taken. Select an empty spot'
+      return false
+    end
+    true
   end
 
   def winner?(choices)
@@ -51,6 +119,13 @@ class Board
   end
 end
 
+# This class represents a Player for the Tic tac toe game
+#
+# Players have name, sym (X or O), and choices (moves they've previously made) attributes
+# To create a player, pass in a name, and then you can seperately update their symbol.
+# Example usage:
+#   player_one = Player.new("Sam")
+#   player_one.sym = "X"
 class Player
   attr_accessor :name, :choices, :sym
 
@@ -61,51 +136,34 @@ class Player
   end
 
   def move
-    gets.chomp.to_i
+    input = gets.strip.chomp
+    return input.to_i if input.match?(/\A[0-8]\z/)
+
+    puts 'Invalid input.'
+    -1
   end
 end
 
-b = Board.new
-b.print_board
-t = Tictactoe.new
-puts "\nEnter player 1's name"
-p1 = Player.new(t.input_player_name)
-p "Enter player 2's name"
-p2 = Player.new(t.input_player_name)
-
-# Determine who goes first and assign their symbol
-if rand(2) == 1
-  p1.sym = 'X'
-  p2.sym = 'O'
-  player_turn = 1
-  puts "Player 1 #{p1.name} goes first with X"
-else
-  p1.sym = 'O' 
-  p2.sym = 'X'
-  player_turn = 2
-  puts "Player 2 #{p2.name} goes first with X"
-end
-
-until t.game_over || t.turns >= 9
-  if player_turn == 1
-    puts "Player 1 #{p1.name}'s turn."
-    move = p1.move
-    move = p1.move until b.valid_move?(move)
-    b.update_board(move,p1.sym)
-    p1.choices.push(move)
-    t.turns += 1
-    b.print_board
-    b.winner?(p1.choices) ? (puts "#{p1.name} wins! Play Again?"; t.game_over = true) : player_turn = 2
-    # player_turn = 2
-  else
-    puts "Player 2 #{p2.name}'s turn."
-    move = p2.move
-    move = p2.move until b.valid_move?(move)
-    b.update_board(move, p2.sym)
-    p2.choices.push(move)
-    t.turns += 1
-    b.print_board
-    b.winner?(p2.choices) ? (puts "#{p2.name} wins! Play Again?"; t.game_over = true) : player_turn = 1
+# module Game is a driver to control the flow of the Tictactoe game
+# Its purpose is to start a new tictactoe game and replay based on user input
+# Example usage:
+#   Game.start
+module Game 
+  def self.replay?
+    response = ''
+    response = gets.strip.chomp while response.empty?
+    response.downcase == 'y' || response.downcase == 'yes'
   end
-  t.turns >= 9 ? "\nDraw! No winner :(. Play Again?" : 'Play Again?'
+
+  def self.start
+    play_again = true
+    while play_again
+      t = Tictactoe.new
+      t.setup
+      t.play until t.game_over
+      play_again = replay?
+    end
+  end
 end
+
+Game.start
